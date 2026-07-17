2e2e482697dc93a7a8aa179caa06e5c0cbd69db1
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-07-17 15:09:07 -0300

    Se agregaron los módulos para nuevo turno y nueva historia en un submenu de más opciones

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ init(autoreset=True)
 
 print()
 print("1. Agregar nuevo paciente")
+print("2. Más opciones")
 print("Salir")
 print()
 
@@ -11,9 +12,42 @@ opcion_menu: str = input("Elija una opción del menú: ").strip()
 
 match opcion_menu:
     
+    #### Menu principal CRUD
+    
     case "1":
         nuevo_paciente()
+    
+    case "2":
+        
+        #### Mas opciones: Submenu para turnos e historias_clinicas
+
+        print()
+        print("1. Nuevo turno")
+        print("2. Historia clinica")
+        print("Salir")
+        print()
+
+        opcion_submenu: str = input("Elija una opción del menú:  ").strip().title()
+
+        match opcion_submenu:
+            case "1":
+                print()
+                print("====NUEVO TURNO MÉDICO ====")
+                print()
+                
+                nuevo_turno()
+
+            case "2":
+                print()
+                print("==== HISTORIAS CLÍNICAS ====")
+                print()
+                
+                nueva_historia()
+
+            case _:
+                print("=== Opción no válida ===")
+
     case _:
-        print("=== Opción inválida ===")
+        print("=== Opción no válida ===")
 
 conexion.close()
