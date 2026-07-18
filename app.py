e7193f46b9cf90b70a54480e39ee2c355a113485
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-07-18 19:35:19 -0300

    Mejora del menú de usuario, se agregaron las secciones de profesional, administrativo y pacientes. se menjoro la distribución de los submenus de cada secciona para una mejor esperiencia de usuario

--- app.py
+++ app.py
@@ -2,52 +2,161 @@ from logica_sistema import *
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
+
+### MENÚ PRINCIPAL
+
 print()
-print("1. Agregar nuevo paciente")
-print("2. Más opciones")
-print("Salir")
+print("1. Area profesional")
+print("2. Area paciente")
+print("3. Area administrativa")
+print("4. Salir")
 print()
 
 opcion_menu: str = input("Elija una opción del menú: ").strip()
 
+### MENÚ PROFESIONAL
+
 match opcion_menu:
     
-    #### Menu principal CRUD
-    
     case "1":
-        nuevo_paciente()
-    
-    case "2":
+        print()
+        print("==== AREA PROFESIONAL ====")
+        print()
+        print("==== MENÚ ====")
+        print()
+        print("1. Agregar turno")
+        print("2. Ver paciente")
+        print("3. Actualizar historia")
+        print("4. Eliminar paciente")
+        print("5. Más opciones")
+        print("6. Salir")
+        print()
         
-        #### Mas opciones: Submenu para turnos e historias_clinicas
-
-        print()
-        print("1. Nuevo turno")
-        print("2. Historia clinica")
-        print("Salir")
+        opcion_submenu_profesional: str = input("Elija una opción del menú:  ").strip().title()
         print()
 
-        opcion_submenu: str = input("Elija una opción del menú:  ").strip().title()
-
-        match opcion_submenu:
+        match opcion_submenu_profesional:
             case "1":
                 print()
                 print("====NUEVO TURNO MÉDICO ====")
                 print()
                 
                 nuevo_turno()
-
+            
             case "2":
                 print()
-                print("==== HISTORIAS CLÍNICAS ====")
+                print("==== VER PACIENTE ====")
                 print()
                 
-                nueva_historia()
-
+                ver_pacientes_profesional()
+            
+            case "3":
+                print()
+                print("==== ACTUALIZAR HISTORIA ====")
+                print()
+                
+                print("Opción en desarrollo...")
+                
+            case "4":
+                print()
+                print("==== ELIMINAR PACIENTE ====")
+                print()
+                
+                print("Opción en desarrollo...")
+                
+                ## MAS OPCIONES
+            
+            case "5":
+                
+                print()
+                print("1. Nueva historia")
+                print("2. Nuevo paciente")
+                print("Salir")
+                print()
+                
+                mas_opciones_prof: str = input("Elija una opción: ").strip()
+                print()
+                
+                match mas_opciones_prof:
+                    case "1":
+                        print()
+                        print("==== NUEVA HISTORIA ====")
+                        print()
+                        
+                        nueva_historia()
+                    
+                    case "2":
+                        print()
+                        print("==== NUEVO PACIENTE ====")
+                        print()
+                        
+                        nuevo_paciente()
+                    
+            case "6":
+                print("Sistema cerrado...")
+            
             case _:
                 print("=== Opción no válida ===")
+                
+    
+### MENÚ PACIENTE
+    
+    case "2":
+        print()
+        print("==== AREA PACIENTE ====")
+        print()
+        print("==== MENÚ ====")
+        print()
+        print("1. Ver turno")
+        print("2. Salir")
+        print()
+        
+        opcion_submenu_paciente: str = input("Elija una opción del menú:  ").strip().title()
+        print()
+    
+        match opcion_submenu_paciente:
+            case "1":
+                print()
+                print("==== VER TURNO ====")
+                print()
+                
+                print("Opción en desarrollo...")
+            
+            case "2":
+                print("Sistema cerrado...")
+                
 
+### MENÚ ADMINISTRATIVO
+
+    case "3":
+        
+        print()
+        print("==== AREA ADMINISTRATIVA ====")
+        print()
+        print("==== MENÚ ====")
+        print()
+        print("1. Nuevo profesional")
+        print("2. Salir")
+        print()
+        
+        opciones_admin: str = input("Elija una opción: ").strip()
+        print()
+        
+        match opciones_admin:
+            case "1":
+                print()
+                print("==== AGREGAR NUEVO PROFESIONAL ====")
+                print()
+                
+                nuevo_profesional()
+                
+            case "2":
+                print("Sistema cerrado...")
+                
+    case "4":
+        print("Sistema cerrado...")
+        
     case _:
         print("=== Opción no válida ===")
-
+        
 conexion.close()
