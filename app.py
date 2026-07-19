33fd01986fe7783e0a2778c14f0ac1866ff9aa0b
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-07-19 13:36:36 -0300

    Se modifico la posicion en el menu para nuevo turno y nuevo paciente al area administrativa, ademas se elimino para el area del profesional la eliminacion del paciente para mantener un historial medico

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from logica_sistema import *
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
-
 ### MENÚ PRINCIPAL
 
 print()
 print("1. Area profesional")
 print("2. Area paciente")
 print("3. Area administrativa")
-print("4. Salir")
+print("4. Más opciones")
+print("5. Salir")
 print()
 
 opcion_menu: str = input("Elija una opción del menú: ").strip()
@@ -24,12 +24,10 @@ match opcion_menu:
         print()
         print("==== MENÚ ====")
         print()
-        print("1. Agregar turno")
-        print("2. Ver paciente")
-        print("3. Actualizar historia")
-        print("4. Eliminar paciente")
-        print("5. Más opciones")
-        print("6. Salir")
+        print("1. Ver paciente")
+        print("2. Actualizar historia")
+        print("3. Más opciones")
+        print("4. Salir")
         print()
         
         opcion_submenu_profesional: str = input("Elija una opción del menú:  ").strip().title()
@@ -38,40 +36,25 @@ match opcion_menu:
         match opcion_submenu_profesional:
             case "1":
                 print()
-                print("====NUEVO TURNO MÉDICO ====")
-                print()
-                
-                nuevo_turno()
-            
-            case "2":
-                print()
                 print("==== VER PACIENTE ====")
                 print()
                 
                 ver_pacientes_profesional()
             
-            case "3":
+            case "2":
                 print()
                 print("==== ACTUALIZAR HISTORIA ====")
                 print()
                 
                 print("Opción en desarrollo...")
                 
-            case "4":
-                print()
-                print("==== ELIMINAR PACIENTE ====")
-                print()
-                
-                print("Opción en desarrollo...")
-                
                 ## MAS OPCIONES
             
-            case "5":
+            case "3":
                 
                 print()
                 print("1. Nueva historia")
-                print("2. Nuevo paciente")
-                print("Salir")
+                print("2. Salir")
                 print()
                 
                 mas_opciones_prof: str = input("Elija una opción: ").strip()
@@ -86,13 +69,9 @@ match opcion_menu:
                         nueva_historia()
                     
                     case "2":
-                        print()
-                        print("==== NUEVO PACIENTE ====")
-                        print()
-                        
-                        nuevo_paciente()
+                        print("Sistema cerrado...")
                     
-            case "6":
+            case "4":
                 print("Sistema cerrado...")
             
             case _:
@@ -120,7 +99,7 @@ match opcion_menu:
                 print("==== VER TURNO ====")
                 print()
                 
-                print("Opción en desarrollo...")
+                ver_turno_pac()
             
             case "2":
                 print("Sistema cerrado...")
@@ -135,8 +114,9 @@ match opcion_menu:
         print()
         print("==== MENÚ ====")
         print()
-        print("1. Nuevo profesional")
-        print("2. Salir")
+        print("1. Nuevo turno medico")
+        print("2. Nuevo paciente")
+        print("3. Salir")
         print()
         
         opciones_admin: str = input("Elija una opción: ").strip()
@@ -145,15 +125,29 @@ match opcion_menu:
         match opciones_admin:
             case "1":
                 print()
-                print("==== AGREGAR NUEVO PROFESIONAL ====")
+                print("====NUEVO TURNO MÉDICO ====")
                 print()
                 
-                nuevo_profesional()
+                nuevo_turno()
                 
             case "2":
+                print()
+                print("==== NUEVO PACIENTE ====")
+                print()
+                
+                nuevo_paciente()
+                
+            case "3":
                 print("Sistema cerrado...")
                 
     case "4":
+        print()
+        print("==== AGREGAR NUEVO PROFESIONAL ====")
+        print()
+                
+        nuevo_profesional()
+    
+    case "5":
         print("Sistema cerrado...")
         
     case _:
